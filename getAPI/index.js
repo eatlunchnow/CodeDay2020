19ce7229b9c8c2847c895eae0156d605078301d0
eatlunchnow <eatlunchnow@gmail.com>
2020-07-14 16:55:21 -0700

    Updated key and updated .gitignore

--- getAPI/index.js
+++ getAPI/index.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./service_key_r.json');
+const serviceAccount = require('./service_key_n.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
