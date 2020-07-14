25a75d180c915ba8b980d11cefd6983e7f053d1a
eatlunchnow <eatlunchnow@gmail.com>
2020-07-13 18:48:34 -0700

    Adding getAPI to read data from task collection

---
+++ getAPI/index.js
@@ -0,0 +1,22 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./service_key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore(); 
+
+
+async function getData(){
+  const taskRef = db.collection('tasks').doc('4eyyOeYNhF1pmfgr4Hp9');
+  const doc = await taskRef.get();
+  if (!doc.exists) {
+    console.log('No such document!');
+  } else {
+    console.log('Document data:', doc.data());
+  }
+
+}
+getData()  
