b2c6e97327fe6a0c14b7c8b7b88cbc887e7a6e0f
eatlunchnow <eatlunchnow@gmail.com>
2020-07-21 13:04:04 -0700

    Added all three get functions to return all volunteers, seniors, and tasks

--- getAPI/index.js
+++ getAPI/index.js
@@ -1,3 +1,6 @@
+// node.js
+
+// database connection
 const admin = require('firebase-admin');
 
 const serviceAccount = require('./service_key_n.json');
@@ -8,15 +11,47 @@ admin.initializeApp({
 
 const db = admin.firestore(); 
 
-
-async function getData(){
-  const taskRef = db.collection('tasks').doc('4eyyOeYNhF1pmfgr4Hp9');
-  const doc = await taskRef.get();
-  if (!doc.exists) {
-    console.log('No such document!');
-  } else {
-    console.log('Document data:', doc.data());
+// functions to read data
+// get data from tasks collection
+async function getTasks(){
+  //const text = document.getElementById('text')
+  const snapshot = await db.collection('tasks').get();
+  if (snapshot.empty) {
+    console.log('No matching documents.');
+    return; 
+  }  
+  snapshot.forEach((doc) => {
+    console.log('Tasks: ', doc.id, '=>', doc.data());
+  });
   }
+  getTasks()
 
-}
-getData()  
+  // get data from seniors collection
+  async function getSeniors(){
+    const snapshot = await db.collection('seniors').get();
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+    snapshot.forEach((doc) => {
+      console.log('Senior: ', doc.id, '=>', doc.data());
+    });
+    }
+    getSeniors()
+
+    // get data from volunteers collection
+    async function getVols(){
+      const snapshot = await db.collection('volunteers').get();
+      if (snapshot.empty) {
+        console.log('No matching documents.');
+        return;
+      }  
+      snapshot.forEach((doc) => {
+        console.log('Volunteer: ', doc.id, '=>', doc.data());
+      });
+      }
+      getVols()
+ 
+
+    
+
